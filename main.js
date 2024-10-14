36c89e81b2ee5270fecb7d583d2d8c8107ea43bc
christianzubcic <christian@burnsred.com.au>
2024-10-14 11:06:12 +1100

    Change marker location

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ window.onload = () => {
         const places = [
             {
                 name: "Landmark 1",
-                latitude: currentLatitude + 0.001,
-                longitude: currentLongitude + 0.001,
+                latitude: currentLatitude + 0.1,
+                longitude: currentLongitude + 0.1,
                 image: 'assets/cube-logo-100.png'
             }
         ];
